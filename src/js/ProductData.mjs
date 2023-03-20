06d78444f85812fae34f0c8dbaf06ba8c0bb34b2
MosesEtte0721 <ett20002@byui.edu>
2023-03-20 14:53:05 +0100

    initial

--- src/js/ProductData.mjs
+++ src/js/ProductData.mjs
@@ -1,3 +1,5 @@
+const baseURL = import.meta.env.VITE_SERVER_URL
+
 function convertToJson(res) {
   if (res.ok) {
     return res.json();
@@ -11,10 +13,10 @@ export default class ProductData {
     this.category = category;
     this.path = `../json/${this.category}.json`;
   }
-  getData() {
+  async getData() {
     return fetch(this.path)
-      .then(convertToJson)
-      .then((data) => data);
+    .then(convertToJson)
+    .then(data => data)
   }
   async findProductById(id) {
     const products = await this.getData();
